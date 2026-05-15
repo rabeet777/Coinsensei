2a71d411b2371d6118fe65774329585441ddaafe
Rabeet Ali <rabeetali145@gmail.com>
2026-05-15 23:16:52 +0500

    meta taf

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={`${manrope.variable} ${inter.variable}`}>
       <head>
+        <meta
+          name="facebook-domain-verification"
+          content="oyxvmp766k71l9wv8gop2j5y9ssfk9"
+        />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block"
